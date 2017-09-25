f7c649bf1e3f396a6ab0a26af43d000ebc161db4
tehnn <tehnnn@gmail.com>
2017-09-25 21:18:44 +0700

    Mon 09/25/2017 21:18:44.17

--- routes/login.js
+++ routes/login.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
+var md5 = require('md5');
 
 router.get('/', (req, res) => {
     res.render('login');
 });
 
 router.post('/', (req, res, next) => {
-    if (req.body.username === 'admin' && req.body.password === '123') {
+    if (req.body.username === 'admin' && md5(req.body.password) === 'ad8680d710ff3257bbf9fd240a7dd9b3') {
         req.session.user = 'admin';
         res.redirect('/');
         return;
