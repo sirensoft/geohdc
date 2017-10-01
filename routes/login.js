9772a66eeacd5184b7118fccf6f7d1b16a99255d
tehnn <tehnnn@gmail.com>
2017-10-01 10:07:06 +0700

    Sun 10/01/2017 10:07:06.34

--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
 var md5 = require('md5');
+var hash = require('password-hash');
 var db_config = require('../config/connect-gis');
+var config = require('../config/config-main');
 
 var knex = require('knex')({
     client: 'mysql',
@@ -14,21 +16,41 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res, next) => {
-    knex('sys_admin').where({
-            username: req.body.username,
-            password: md5(req.body.password)
-        }).count('username as ok')
-        .then((raw) => {
-            //console.log(raw[0].ok * 1);
-            if (raw[0].ok * 1 > 0) {
+    if (config.db === 'hdc') {
+        knex('sys_admin').where({
+                username: req.body.username,
+                password: md5(req.body.password)
+            }).count('username as ok')
+            .then((raw) => {
+                //console.log(raw[0].ok * 1);
+                if (raw[0].ok * 1 > 0) {
 
-                req.session.user = req.body.username;
-                res.redirect('/');
-                return;
-            }
-            res.redirect('/login');
-        });
+                    req.session.user = req.body.username;
+                    res.redirect('/');
+                    return;
+                }
+                res.redirect('/login');
+            });
+        return;
+    } else {
+        console.log("h=" + hash.generate(req.body.password));
+        knex('user').where({
+                username: req.body.username,
+                //password_hash: hash.generate(req.body.password)
+            }).count('username as ok')
+            .then((raw) => {
+                //console.log(raw[0].ok * 1);
+                if (raw[0].ok * 1 > 0) {
 
+                    req.session.user = req.body.username;
+                    res.redirect('/');
+                    return;
+                }
+                res.redirect('/login');
+            });
+        return;
+
+    }
 });
 router.get('/logout', (req, res) => {
 
