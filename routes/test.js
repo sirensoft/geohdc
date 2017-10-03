a5eb9e50cd32c2df41effba8e55ea53ef3fa3f60
tehnn <tehnnn@gmail.com>
2017-10-03 13:23:12 +0700

    Tue 10/03/2017 13:23:12.79

--- routes/test.js
+++ routes/test.js
@@ -7,6 +7,9 @@ var json2csv = require('json2csv');
 
 var json_village = require('../data/village');
 
+router.get('/', (req, res) => {
+    res.render('test');
+});
 router.get('/json', (req, res) => {
 
     var p = req.query.p;
@@ -91,6 +94,30 @@ router.get('/vill/get/:p', (req, res) => {
     var csv = json2csv({ data: datas, quotes: '' });
     res.attachment('mooban.txt');
     res.status(200).send(csv);
+
+});
+
+router.get('/chart', function(req, res) {
+
+
+    var data = [{
+        name: 'DHF',
+        data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+    }, {
+        name: 'DF',
+        data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
+    }, {
+        name: 'DSS',
+        data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
+    }, {
+        name: 'Zika',
+        data: [15000, 10000, 7988, 12169, 15112, 22452, 34400, 34227]
+    }];
+
+
+
+    res.json(data);
+
 
 });
 
