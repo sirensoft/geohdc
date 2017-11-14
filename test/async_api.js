aba15885c0f00d068311256e9976c0010545a4d4
tehnn <tehnnn@gmail.com>
2017-11-14 11:12:12 +0700

    Tue 11/14/2017 11:12:12.56

--- test/async_api.js
+++ test/async_api.js
@@ -1,5 +1,12 @@
 var express = require('express');
 var app = new express();
+var db_config = require('../config/connect-gis');
+
+
+var knex = require('knex')({
+    client: 'mysql',
+    connection: db_config
+});
 
 
 function delay(id) {
@@ -18,7 +25,7 @@ delay(1).then(function(res) {
     console.log(res);
 });
 
-async function show(){
+async function show() {
     var a = await delay(200);
     console.log(a);
 }
@@ -26,10 +33,18 @@ show();
 
 
 
-
 app.get('/', async function(req, res) {
     var a = await delay(10);
     res.send('OK...' + a);
 });
+
+app.get('/db', async(req, res) => {
+    var sql = "select * from user";
+    var rows = await knex.raw(sql);
+    res.json(rows[0]);
+
+});
+
+
 
 app.listen(88);
