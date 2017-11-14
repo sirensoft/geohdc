7d0afeff5cbb28567cd25b1805d56abf49407ea5
tehnn <tehnnn@gmail.com>
2017-11-15 00:17:18 +0700

    Wed 11/15/2017  0:17:18.36

---
+++ test/login.js
@@ -0,0 +1,57 @@
+var express = require('express')
+var session = require('express-session')
+var path = require('path')
+var bodyParser = require('body-parser')
+
+var app = new express()
+
+app.set('views', path.join(__dirname, 'login'));
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(session({
+    secret: 'superkey',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}))
+
+
+app.get('/login', function(req, res) {
+    res.render('login')
+})
+app.post('/login', function(req, res) {
+    if (req.body.username.trim()) {
+        req.session.logged = true
+        req.session.username = req.body.username
+        res.redirect('/')
+    } else {
+        res.redirect('/login')
+    }
+
+})
+
+app.get('/logout', function(req, res) {
+    req.session.destroy()
+    res.send('Log Out...ok')
+})
+var auth = function(req, res, next) {
+    if (req.session.logged) {
+        next()
+    } else {
+        res.redirect('/login')
+    }
+}
+
+app.use(auth)
+
+app.get('/', function(req, res) {
+    res.send('First page for...' + req.session.username)
+})
+
+app.get('/person', function(req, res) {
+    res.status(200).json([{ name: 'a', age: 26 }, { name: 'b', age: 26 }])
+})
+
+app.listen(88)
