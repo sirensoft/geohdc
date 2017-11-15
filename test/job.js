2b3ccba25a6c141e72592a272787f8a60784ac50
tehnn <tehnnn@gmail.com>
2017-11-15 09:25:17 +0700

    Wed 11/15/2017  9:25:16.89

--- test/job.js
+++ test/job.js
@@ -1,13 +1,21 @@
 var express = require('express')
 var router = express.Router();
+var user = require('./models/user')
+var mUser = new user();
 
 router.get('/', function(req, res) {
     res.send('First page for...' + req.session.username)
 })
 
-router.get('/person', function(req, res) {
-    let persons = [{ name: 'aaaaaa', age: 26 }, { name: 'bbbbb', age: 20 }]
-    res.render('person', { persons: persons })
+router.get('/person', async function(req, res) {
+    let rows = await mUser.list()
+    res.render('person', { persons: rows[0] })
+})
+
+router.get('/person2', (req, res) => {
+    mUser.list().then(function(rows) {
+        res.render('person', { persons: rows[0] });
+    })
 })
 
 module.exports = router
---
+++ test/models/user.js
@@ -0,0 +1,15 @@
+var db_config = require('../../config/connect-gis');
+
+var knex = require('knex')({
+    client: 'mysql',
+    connection: db_config
+});
+
+class user {
+    list() {
+        let sql = "select id,username,email from user"
+        return knex.raw(sql)
+    }
+}
+
+module.exports = user;
